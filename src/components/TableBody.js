9bd878135d5edb8c77247e9914922e20faed1a8b
Karthikeya Varma <karthikeyavarma73@gmail.com>
2023-09-24 23:36:32 +0530

    Sorted the table

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -6,6 +6,24 @@ function TableBody({ Participationdata,
     setParticipationdata }) {
     // console.log(Participationdata)
 
+    Participationdata.sort((i,j) => {
+        if(i["Total Completions of both Pathways"] === "No" && j["Total Completions of both Pathways"] === "Yes")
+            return 1
+        else if(i["Total Completions of both Pathways"] === "Yes" && j["Total Completions of both Pathways"] === "No")
+            return -1
+        else
+            return 0
+    })
+    Participationdata.sort((i,j) => {
+        if(i["Redemption Status"] === "No" && j["Redemption Status"] === "Yes")
+            return 1
+        else if(i["Redemption Status"] === "Yes" && j["Redemption Status"] === "No")
+            return -1
+        else
+            return 0
+    })
+
+
 
     return (
         <tbody className='text-xs '>
